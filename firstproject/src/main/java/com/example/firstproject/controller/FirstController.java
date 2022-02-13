18793338d4454bcdf22fb8d895d3d0dbae914c38
kmj91 <lethita0302@gmail.com>
2022-02-13 21:46:23 +0900

    뷰 템플릿

---
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,16 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model _model){
+        // 변수 전달
+        _model.addAttribute("username", "연습");
+        // templates/greetings.mustache -> 브라우저로 전송
+        return "greetings";
+    }
+}
